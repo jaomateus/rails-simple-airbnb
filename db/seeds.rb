3fb814ed7239bc0cd0942d6e07bb65c2e298e5e0
Joao Mateus <jaomateus@gmail.com>
2023-02-21 22:41:21 +0100

    update exercise

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+puts "Deleting flats..."
+Flat.destroy_all
+
+puts 'Creating flats...'
+
+10.times do 
+  Flat.create!(
+    name: Faker::TvShows::RuPaul.queen,
+    address: Faker::Address.full_address,
+    description: Faker::Markdown.emphasis,
+    price_per_night: [35, 45, 60, 75, 40, 80].sample,
+    number_of_guests: [1, 2, 3, 4, 5].sample
+)
+end
+
+
